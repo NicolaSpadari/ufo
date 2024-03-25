c2f7de7d93ccd8f7a6f4acaad442af79cacf0920
NicolaSpadari <spadari.nicola@gmail.com>
2024-03-25 16:52:36 +0100

    Add debug component

---
+++ components/Debug.vue
@@ -0,0 +1,17 @@
+<template>
+	<div fixed bottom-0 right-0 flex flex-col bg-gray-200 space-y-3>
+		<button @click="initializeClient()">
+			initializeClient
+		</button>
+		<button @click="setSubscriptions()">
+			setSubscriptions
+		</button>
+		<button @click="setMultireddits()">
+			setMultireddits
+		</button>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	const { initializeClient, setSubscriptions, setMultireddits } = useReddit();
+</script>
