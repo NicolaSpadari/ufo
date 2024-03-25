e925f9757a95f678b5be187942bbe4c0069ea3e7
NicolaSpadari <spadari.nicola@gmail.com>
2024-03-25 16:52:55 +0100

    Add production url constant

--- composables/constants.ts
+++ composables/constants.ts
@@ -4,12 +4,14 @@ export const useConstants = () => {
 	const allScopes = ["identity", "edit", "flair", "history", "mysubreddits", "privatemessages", "read", "report", "save", "submit", "subscribe", "vote"];
 	const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
 	const batchSize = 15;
+	const productionUrl = "https://ufo.nicolaspadari.dev";
 
 	return {
 		appName,
 		randomString,
 		allScopes,
 		userAgent,
-		batchSize
+		batchSize,
+		productionUrl
 	};
 };
