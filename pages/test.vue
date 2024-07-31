68e95326def18fac8f4ce3784ba06fa8c4ff8940
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-07-31 15:31:18 +0200

    animate v2 sidebar

--- pages/test.vue
+++ pages/test.vue
@@ -2,11 +2,29 @@
 	<div>
 		<V2Navbar />
 		<div grid grid-cols-1 lg="grid-cols-12" gap-3 p-6>
-			<div lg="col-span-3" border>
-				<V2Sidebar />
+			<div
+				lg="col-span-3 translate-x-0" class="<lg:fixed" relative z-10 transform transition duration-500 ease-in-out
+				:class="sidebar ? '' : '-translate-x-full'"
+			>
+				<div class="h-full flex flex-col overflow-y-auto bg-white">
+					<div class="px-4 sm:px-6" lg="hidden">
+						<div class="flex items-start justify-between">
+							<div class="ml-3 h-7 flex items-center">
+								<button type="button" @click="sidebar = !sidebar" class="relative rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+									<span class="absolute -inset-2.5" />
+									<Icon name="heroicons-outline:x-mark" size-8 />
+								</button>
+							</div>
+						</div>
+					</div>
+					<div class="flex-1 px-4 sm:px-6">
+						<V2Sidebar />
+					</div>
+				</div>
 			</div>
+
 			<div lg="col-span-6" border>
-				page
+				content
 			</div>
 			<div lg="col-span-3" border>
 				other sidebar
@@ -19,6 +37,8 @@
 	definePageMeta({
 		layout: "blank"
 	});
+
+	const sidebar = ref(false);
 </script>
 
 <style>
