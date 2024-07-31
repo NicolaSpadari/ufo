f7aa2b368edfba1c61ceda6501d74a131d2f75ac
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-07-31 14:41:03 +0200

    component cleanup

--- pages/test.vue
+++ pages/test.vue
@@ -25,19 +25,10 @@
 					<button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
 						<span class="absolute -inset-0.5" />
 						<span class="sr-only">Open menu</span>
-						<!--
-            Icon when menu is closed.
 
-            Menu open: "hidden", Menu closed: "block"
-          -->
 						<svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 						</svg>
-						<!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
 						<svg class="h-6 w-6 hidden" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
 							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 						</svg>
@@ -61,72 +52,10 @@
 								<img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
 							</button>
 						</div>
-
-						<!--
-            Dropdown menu, show/hide based on menu state.
-
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          -->
-						<div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-							<!-- Active: "bg-gray-100", Not Active: "" -->
-							<a id="user-menu-item-0" href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Your Profile</a>
-							<a id="user-menu-item-1" href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Settings</a>
-							<a id="user-menu-item-2" href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1">Sign out</a>
-						</div>
 					</div>
 				</div>
 			</div>
-			<nav class="hidden lg:flex lg:py-2 lg:space-x-8" aria-label="Global">
-				<!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
-				<a href="#" class="inline-flex items-center rounded-md bg-gray-100 px-3 py-2 text-sm text-gray-900 font-medium" aria-current="page">Dashboard</a>
-				<a href="#" class="inline-flex items-center rounded-md px-3 py-2 text-sm text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Team</a>
-				<a href="#" class="inline-flex items-center rounded-md px-3 py-2 text-sm text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Projects</a>
-				<a href="#" class="inline-flex items-center rounded-md px-3 py-2 text-sm text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Calendar</a>
-			</nav>
 		</div>
-
-		<!-- Mobile menu, show/hide based on menu state. -->
-		<nav id="mobile-menu" class="lg:hidden" aria-label="Global">
-			<div class="px-2 pb-3 pt-2 space-y-1">
-				<!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-900 hover:bg-gray-50 hover:text-gray-900" -->
-				<a href="#" class="block rounded-md bg-gray-100 px-3 py-2 text-base text-gray-900 font-medium" aria-current="page">Dashboard</a>
-				<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Team</a>
-				<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Projects</a>
-				<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-900 font-medium hover:bg-gray-50 hover:text-gray-900">Calendar</a>
-			</div>
-			<div class="border-t border-gray-200 pb-3 pt-4">
-				<div class="flex items-center px-4">
-					<div class="flex-shrink-0">
-						<img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-					</div>
-					<div class="ml-3">
-						<div class="text-base text-gray-800 font-medium">
-							Tom Cook
-						</div>
-						<div class="text-sm text-gray-500 font-medium">
-							tom@example.com
-						</div>
-					</div>
-					<button type="button" class="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-						<span class="absolute -inset-1.5" />
-						<span class="sr-only">View notifications</span>
-						<svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-							<path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-						</svg>
-					</button>
-				</div>
-				<div class="mt-3 px-2 space-y-1">
-					<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-500 font-medium hover:bg-gray-50 hover:text-gray-900">Your Profile</a>
-					<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-500 font-medium hover:bg-gray-50 hover:text-gray-900">Settings</a>
-					<a href="#" class="block rounded-md px-3 py-2 text-base text-gray-500 font-medium hover:bg-gray-50 hover:text-gray-900">Sign out</a>
-				</div>
-			</div>
-		</nav>
 	</header>
 </template>
 
