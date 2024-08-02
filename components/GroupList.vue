7779e521d97a7b4559a9ff5da22bba81f4b0d4b6
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-08-02 17:56:39 +0200

    Minor grouplist fixes

--- components/GroupList.vue
+++ components/GroupList.vue
@@ -14,27 +14,28 @@
 				/>
 			</AccordionTrigger>
 		</AccordionHeader>
-		<AccordionContent class="overflow-hidden bg-mauve2 text-[15px] text-mauve11 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown" animate-duration="300!">
-			<ul v-if="subredditList" px-5 py-4 space-y-1>
-				<li v-for="entry in subredditList" :key="entry.name" rounded-md hover="bg-mauve4">
-					<NuxtLink :to="`/${entry.display_name_prefixed.toLowerCase()}`" flex items-center gap-3 rounded-lg px-4 py-2 text-sm text-light font-medium hover="bg-light/15">
-						<SubredditIcon :image="entry.icon_img" size="small" />
-						<span text-dark-800 font-text>
-							{{ entry.display_name_prefixed }}
-						</span>
-					</NuxtLink>
-				</li>
-			</ul>
-
-			<ul v-if="multiredditList" px-5 py-4 space-y-1>
-				<li v-for="entry in multiredditList" :key="entry.name" rounded-md hover="bg-mauve4">
-					<NuxtLink :to="`/m/${entry.name}`" flex items-center gap-3 rounded-lg px-4 py-2 text-sm text-light font-medium hover="bg-light/15">
-						<SubredditIcon :image="entry.icon_url" size="small" />
-						<span text-dark-800 font-text>
-							{{ entry.display_name }}
-						</span>
-					</NuxtLink>
-				</li>
+		<AccordionContent class="overflow-hidden bg-mauve2 text-[15px] text-mauve11 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown">
+			<ul px-5 py-4 space-y-1>
+				<template v-if="subredditList">
+					<li v-for="entry in subredditList" :key="entry.name" rounded-md hover="bg-mauve4">
+						<NuxtLink :to="`/${entry.display_name_prefixed.toLowerCase()}`" flex items-center gap-3 rounded-lg px-4 py-2 text-sm text-light font-medium hover="bg-light/15">
+							<SubredditIcon :image="entry.icon_img" size="small" />
+							<span text-dark-800 font-text>
+								{{ entry.display_name_prefixed }}
+							</span>
+						</NuxtLink>
+					</li>
+				</template>
+				<template v-if="multiredditList">
+					<li v-for="entry in multiredditList" :key="entry.name" rounded-md hover="bg-mauve4">
+						<NuxtLink :to="`/m/${entry.name}`" flex items-center gap-3 rounded-lg px-4 py-2 text-sm text-light font-medium hover="bg-light/15">
+							<SubredditIcon :image="entry.icon_url" size="small" />
+							<span text-dark-800 font-text>
+								{{ entry.display_name }}
+							</span>
+						</NuxtLink>
+					</li>
+				</template>
 			</ul>
 		</AccordionContent>
 	</AccordionItem>
--- uno.config.ts
+++ uno.config.ts
@@ -58,17 +58,19 @@ export default defineConfig({
 			xl: "1200px",
 			xxl: "1400px"
 		},
-		animation: {
-			keyframes: {
-				slideDown: `{
-					from { height: 0 }
-  					to { height: var(--radix-accordion-content-height) }
-				}`,
-				slideUp: `{
-					from { height: var(--radix-accordion-content-height) }
-  					to { height: 0 }
-				}`
+		keyframes: {
+			slideDown: {
+				from: { height: 0 },
+				to: { height: "var(--radix-accordion-content-height)" }
+			},
+			slideUp: {
+				from: { height: "var(--radix-accordion-content-height)" },
+				to: { height: 0 }
 			}
+		},
+		animation: {
+			slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+			slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)"
 		}
 	},
 	transformers: [
