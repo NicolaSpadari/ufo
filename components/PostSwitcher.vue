a12234e9b0a71be20553ef4b8b48cfcc19bdec40
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-08-05 15:52:39 +0200

    Fix post switcher palette

--- components/PostSwitcher.vue
+++ components/PostSwitcher.vue
@@ -12,7 +12,7 @@
 				<MenubarContent
 					ui-open="animate-slideDownAndFade"
 					ui-closed="animate-slideUpAndFade"
-					will-change="transform,opacity" min-w="220px" rounded-md bg-white p-1 shadow-xl outline-none
+					will-change="transform,opacity" min-w="220px" rounded-md bg-zinc-900 p-1 shadow-xl outline-none
 					align="start"
 					:side-offset="5"
 					:align-offset="-3"
@@ -21,11 +21,14 @@
 						<MenubarRadioItem
 							v-for="mode in availableOrders"
 							:key="mode"
-							class="relative h-[25px] flex pointer select-none items-center rounded-[3px] px-[5px] pl-[20px] text-[13px] text-green-600 leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-green9 data-[disabled]:text-mauve8 data-[highlighted]:text-green1"
+							relative h="30px" flex pointer select-none items-center rounded="3px" pl="25px" text-sm text-green-600 leading-none outline-none class="data-[highlighted]:(bg-green-600 text-zinc-100)"
 							:value="mode"
 						>
-							<MenubarItemIndicator v-if="mode === order" class="absolute left-0 w-[20px] inline-flex items-center justify-center">
-								<Icon name="heroicons-outline:check" />
+							<MenubarItemIndicator
+								v-if="mode === order"
+								absolute left="3px" w="20px" flex-center
+							>
+								<Icon name="heroicons-solid:check" />
 							</MenubarItemIndicator>
 							<span capitalize>{{ mode }}</span>
 						</MenubarRadioItem>
@@ -35,15 +38,17 @@
 		</MenubarMenu>
 		<MenubarMenu v-if="order === 'top'" value="sort">
 			<MenubarTrigger
-				class="flex-center select-none gap-1 rounded px-3 py-2 text-[13px] text-grass11 leading-none outline-none hover:bg-green4"
-				ui-open="bg-green4"
+				flex-center select-none gap-1 rounded px-3 py-2 text-sm text-green-600 leading-none outline-none hover="bg-zinc-800" transition-colors
+				ui-open="bg-zinc-800"
 			>
 				<span>Sort by: </span>
 				<span font-semibold capitalize>{{ sort }}</span>
 			</MenubarTrigger>
 			<MenubarPortal>
 				<MenubarContent
-					class="will-change-[transform,opacity] [animation-duration:_400ms] [animation-timing-function:_cubic-bezier(0.16,_1,_0.3,_1)] min-w-[220px] rounded-md bg-white p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] outline-none"
+					ui-open="animate-slideDownAndFade"
+					ui-closed="animate-slideUpAndFade"
+					will-change="transform,opacity" min-w="220px" rounded-md bg-zinc-900 p-1 shadow-xl outline-none
 					align="start"
 					:side-offset="5"
 					:align-offset="-3"
@@ -52,11 +57,14 @@
 						<MenubarRadioItem
 							v-for="mode in availableSorts"
 							:key="mode"
-							class="relative h-[25px] flex pointer select-none items-center rounded-[3px] px-[5px] pl-[20px] text-[13px] text-grass11 leading-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-green9 data-[disabled]:text-mauve8 data-[highlighted]:text-green1"
+							relative h="30px" flex pointer select-none items-center rounded="3px" pl="25px" text-sm text-green-600 leading-none outline-none class="data-[highlighted]:(bg-green-600 text-zinc-100)"
 							:value="mode"
 						>
-							<MenubarItemIndicator v-if="mode === sort" class="absolute left-0 w-[20px] inline-flex items-center justify-center">
-								<Icon name="heroicons-outline:check" />
+							<MenubarItemIndicator
+								v-if="mode === sort"
+								absolute left="3px" w="20px" flex-center
+							>
+								<Icon name="heroicons-solid:check" />
 							</MenubarItemIndicator>
 							<span capitalize>{{ mode }}</span>
 						</MenubarRadioItem>
