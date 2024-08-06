9ceefed29b4a7b404962238356ce4f20b7c50d1e
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-08-06 11:00:43 +0200

    Fix sidebar z-index

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 		<div bg-zinc-800>
 			<div lg="grid-cols-12" grid grid-cols-1 gap-3 crate>
 				<div
-					lg="col-span-3 translate-x-0 sticky top-0" class="<lg:(fixed left-0 z-10 w-xs transform transition duration-500 ease-in-out)"
+					lg="col-span-3 translate-x-0 sticky top-0" class="<lg:(fixed left-0 z-12 w-xs transform transition duration-500 ease-in-out)"
 					:class="open ? '' : '-translate-x-[105%]'"
 					:style="{
 						top: `${height}px`,
