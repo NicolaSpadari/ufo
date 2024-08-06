f8c7940e0e86ec32f7f43a25eba4cbbc47a7c2e3
Nicola Spadari <NicolaSpadari@users.noreply.github.com>
2024-08-06 11:00:51 +0200

    Add loader in search

--- components/Site/Search.vue
+++ components/Site/Search.vue
@@ -1,9 +1,9 @@
 <template>
 	<div flex-center flex-1 px-2 sm="absolute inset-0">
 		<ComboboxRoot relative>
-			<ComboboxAnchor h="35px" min-w="160px" inline-flex items-center justify-between gap-1 rounded bg-zinc-800 px-4 text-sm text-green-600 leading-none shadow="[0_2px_10px] blackA2" outline-none lg="min-w-[320px]" md="min-w-[240px]">
+			<ComboboxAnchor h="35px" min-w="160px" inline-flex items-center justify-between gap-1 rounded bg-zinc-800 px-4 text-sm text-green-600 leading-none shadow-xl outline-none lg="min-w-[320px]" md="min-w-[240px]">
 				<ComboboxInput
-					h-full text-zinc-400 bg-zinc-800 outline-none selection="bg-green-500" placeholder-zinc-400
+					selection="bg-green-500" size-full bg-zinc-800 text-zinc-400 outline-none placeholder-zinc-400
 					placeholder="Search"
 					@input="setValue($event);debouncedSearch()"
 				/>
@@ -22,14 +22,15 @@
 				absolute z-10 mt-2 min-w="160px" w-full overflow-hidden rounded bg-zinc-800 shadow="[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
 			>
 				<ComboboxViewport p-1>
-					<ComboboxEmpty py-2 text-center text-xs text-mauve8 font-medium />
+					<Loader v-show="loading" my-3 />
+					<ComboboxEmpty v-show="!loading" py-2 text-center text-xs text-mauve8 font-medium />
 
 					<ComboboxGroup>
 						<ComboboxLabel px-6 text-xs text-zinc-400 leading-6>
 							Results
 						</ComboboxLabel>
 
-						<template v-if="results.length">
+						<template v-if="results.length && !loading">
 							<ComboboxItem
 								v-for="(option, index) in results"
 								:key="index"
@@ -51,6 +52,7 @@
 <script lang="ts" setup>
 	const { client } = useReddit();
 	const term = ref("");
+	const loading = ref(false);
 	const results = ref<Subreddit[]>([]);
 
 	const setValue = (event: { target: HTMLInputElement }) => {
@@ -58,10 +60,12 @@
 	};
 
 	const debouncedSearch = useDebounceFn(() => {
+		loading.value = true;
 		client.value?.searchSubreddits({
 			query: term.value
 		}).then((res: Subreddit[]) => {
 			results.value = res;
+			loading.value = false;
 		});
 	}, 1000);
 </script>
